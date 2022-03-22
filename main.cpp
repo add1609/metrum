5ed43d27eb0d39efef787102ec2e0400a2cd2831
add003 <ahmedsami03@protonmail.com>
2022-03-22 21:56:09 +0100

    test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Tokenizer.hpp"
 
 int main() {
+    // Test
     std::string eingabe = "Fremd bin ich eingezogen,\n Fremd zieh ich wieder aus.\n Der Mai war mir gewogen mit manchem Blumenstrauss.";
     std::cout << "\nEingabe:\n" << eingabe << "\n\nAusgabe:\n";
     metrum::Tokenizer::parse(eingabe).debugPrint();
